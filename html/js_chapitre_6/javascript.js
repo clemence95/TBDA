d934fe642f3452be79d152ed8130331ea57ac634
Boitelle <boitelleclemence@outlook.fr>
2023-05-05 09:31:24 +0200

    html

--- html/js_chapitre_6/javascript.js
+++ html/js_chapitre_6/javascript.js
@@ -1,4 +1,19 @@
-var nombre ="100";
-if (nombre=="100"){
-alert("Ceci est une chaine de caractere:"+nombre);
+var a ="100";
+alert("Ceci est une chaine de caractere:"+a);
+
+var b =100;
+// Décrémentation 
+b--;
+console.log(b);
+
+var c =1;
+alert (Number(c)+Number(a));
+
+var d = false;
+if (d){
+    console.log("le booléen est à true");
+}else{
+    console.log("le boléen est à else")
 }
+alert("Voici ce que vaux le boléen :\n\nd :" +d); 
+
