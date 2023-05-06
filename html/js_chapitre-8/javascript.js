e4f4d346715c68ac8764e98dff1942375bbfaf41
boitelle clémence <boitelleclemence@outlook.fr>
2023-05-06 20:32:23 +0200

    html

--- html/js_chapitre-8/javascript.js
+++ html/js_chapitre-8/javascript.js
@@ -1,14 +1,11 @@
-var bouton1= document.getElementById("Id_btn1");
-bouton1.addEventListener("click",clickbtn1);
-
-function clickbtn1(){
-
-    var Nomprenom = prompt("Saisir votre nom");
-    if(Nomprenom==null){
-        alert("Annuler");
-        return;
-    }
-while (
-    
-)
+var Nomprenom = prompt("Saisir votre nom");
+if (Nomprenom == null) {
+    alert("Annuler");
+    return;
 }
+while (true){
+
+    
+}
+
+
