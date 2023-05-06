25d4f8fbefcf353f60c8f1574bbd763f6b49b93f
clemence95 <131000911+clemence95@users.noreply.github.com>
2023-05-07 00:45:39 +0200

    html

--- html/js_chapitre-8/javascript.js
+++ html/js_chapitre-8/javascript.js
@@ -1,10 +1,4 @@
-var Nom = prompt("Saisir votre nom");
+var NomPrenom = prompt("Saisir votre nom");
 if(Nom==null){
     alert("Annuler");
     return;
-}
-var Prenom = prompt("Saisair votre prenom");
-if (Prenom==null){
-alert("Annuler");
-return;
-}
