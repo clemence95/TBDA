dda5db0b4dd813b63381f871efe834086fb660e9
boitelle clémence <boitelleclemence@outlook.fr>
2023-05-08 01:27:42 +0200

    html

--- html/js_chapitre-8/javascript.js
+++ html/js_chapitre-8/javascript.js
@@ -1,4 +1,7 @@
 var prenom = prompt("Saisir un Prénom");
 while(prenom!=null){
     var prenom=prompt("Saisir un prenom");
+    var nombre = 0; ligne=prenom; ligne++;
 }
+console.log("ligne"+prenom+"="+resultat);
+alert("voici ce que vous avez saisi :\n\nNombre de prenom :"+ligne+"\nvotre prenom :" +prenom);
