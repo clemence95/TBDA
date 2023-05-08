570bd8b4367d750f5464ef5f5b23958402c74ea0
boitelle clémence <boitelleclemence@outlook.fr>
2023-05-08 19:14:33 +0200

    html

--- html/js_chapitre-8/javascript.js
+++ html/js_chapitre-8/javascript.js
@@ -1,7 +1,13 @@
-var prenoms = prompt("Saisir un prenoms");
-while(prenoms!=null){
-    if()
-var prenoms = prompt("Saisir prenoms")
-console.log(prenoms);
+var prenom="prenom"; //type "chaine"
+i = 0
+while(prenom!="") // boucle while(condition vraie)
+{// Si la condition (prenom strictement égale à "" Alors;)
+    prenom=prompt("Entrez le prenom N°"+i);//Demande prenom n°1 à l'utilisateur
+    i++; //(i=0)+(i=0) à chaque fois qu'un prenom est saisi
+    console.log(i+prenom);// Afficher sur la console log
+    if(prenom==null){// mais si prenom est égale à null alors
+        alert("Annuler");
+        break;
+    }
 }
-console.log(prenoms);
+
