06088c02c8b7655b8a741799d6658a2b507223ff
boitelle clémence <boitelleclemence@outlook.fr>
2023-05-08 20:53:38 +0200

    html

--- html/js_chapitre-8/javascript.js
+++ html/js_chapitre-8/javascript.js
@@ -1,13 +1,17 @@
-var prenom="prenom"; //type "chaine"
+var prenom = "prenom"; //type "chaine"
 i = 0
-while(prenom!="") // boucle while(condition vraie)
+while (prenom != "") // boucle while(condition vraie)
 {// Si la condition (prenom strictement égale à "" Alors;)
-    prenom=prompt("Entrez le prenom N°"+i);//Demande prenom n°1 à l'utilisateur
+    prenom = prompt("Entrez le prenom N°" + i);//Demande prenom n°1 à l'utilisateur
     i++; //(i=0)+(i=0) à chaque fois qu'un prenom est saisi
-    console.log(i+prenom);// Afficher sur la console log
-    if(prenom==null){// mais si prenom est égale à null alors
+    console.log(i + prenom);// Afficher sur la console log
+    if (prenom == null) {// mais si prenom est égale à null alors
         alert("Annuler");
         break;
     }
+}
+var nombre = prompt("Ecrire un nombre entier");
+for (var i = nombre; i >= 0; i--) {
+    console.log(i);
 }
 
