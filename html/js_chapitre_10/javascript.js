2db135f90122e9331844b93c934a7901191f8643
Boitelle <boitelleclemence@outlook.fr>
2023-05-10 17:54:41 +0200

    html

--- html/js_chapitre_10/javascript.js
+++ html/js_chapitre_10/javascript.js
@@ -1,18 +1,23 @@
-// Demande à l'utilisateur de saisir la taille du tableau
-var tailleTableau = prompt("Entrez la taille du tableau:");
+    // Demande à l'utilisateur de saisir la taille du tableau
+  var tailleTableau = prompt("Entrez la taille du tableau:");
+tailleTableau.length;
+   // Convertit la réponse en nombre entier
+    tailleTableau = parseInt(tailleTableau);
 
-// Convertit la réponse en nombre entier
-tailleTableau = parseInt(tailleTableau);
+  // Initialise le tableau
+    var tableau = [];
 
-// Initialise le tableau
-var tableau = [];
+   // Boucle pour demander à l'utilisateur de saisir les valeurs du tableau
+   for (var i = 0; i < tailleTableau; i++) 
+    tab[tailleTableau] = prompt("Entrez une valeur:");
 
-// Boucle pour demander à l'utilisateur de saisir les valeurs du tableau
-for (var i = 0; i < tailleTableau; i++) 
-var valeur = prompt("Entrez une valeur:");
+    // Ajoute la valeur à la fin du tableau
+     tableau.push(valeur);
 
-// Ajoute la valeur à la fin du tableau
-tableau.push(valeur);
+    // Affiche le contenu du tableau
+    console.table(length);
+   console.table(tableau);
+   alert(tableau);
 
-// Affiche le contenu du tableau
-console.log(tableau);
+
+
