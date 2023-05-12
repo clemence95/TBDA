102a654ffd0fe324ab86b6c90868ff45037d72a4
Boitelle <boitelleclemence@outlook.fr>
2023-05-12 11:42:56 +0200

    html

---
+++ html/evaluation_Javascript/script.js
@@ -0,0 +1,8 @@
+let age = prompt("Saisir un age"); //variable age
+let nbinf20 = 0; //variable age inferieur à 20
+let nbentre24 = 0; // variable supérieure ou égale à 20 et 40
+let sup40= 0; // variable suprieure à 40
+
+while()
+
+
