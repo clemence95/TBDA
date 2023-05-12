161adcbcb9ca7f80e63ebcb64fa63d6cf0ea4508
Boitelle <boitelleclemence@outlook.fr>
2023-05-12 12:17:09 +0200

    html

--- html/evaluation_Javascript/script.js
+++ html/evaluation_Javascript/script.js
@@ -1,8 +1,23 @@
-let age = prompt("Saisir un age"); //variable age
-let nbinf20 = 0; //variable age inferieur à 20
-let nbentre24 = 0; // variable supérieure ou égale à 20 et 40
-let sup40= 0; // variable suprieure à 40
+let age = prompt(perseInt("Saisir un age")); //variable age
+let jeune = 0; //variable age inferieur à 20
+let adulte = 0; // variable supérieure ou égale à 20 et 40
+let centenaire= 0; // variable suprieure à 4
 
-while()
+while(age<!20){
+jeune++;
+let age = prompt(perseInt("Saisir un age"));
+if(age>=20 && age>=40){
+    adulte++;
+     age = prompt(perseInt("Saisir un age"));
+}
+else if(age>!40){
+    centenaire++;
+    break;
+}
+}
+document.write("Nombre de personnes de moins de 20 ans : " +jeune+"<br>");
+document.write("Nombre de personnes entre 20 et 40 ans : " +adulte+"<br>");
+document.write("Nombre de personnes de plus de 40 ans : " +centenaire+"<br><br>");
+
 
 
