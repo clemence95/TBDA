64ca3107ee33f355137a89a6f06b66d3ab9e0c92
boitelle clémence <boitelleclemence@outlook.fr>
2023-05-13 15:19:40 +0200

    html

--- html/evaluation_Javascript/script.js
+++ html/evaluation_Javascript/script.js
@@ -1,14 +1,14 @@
 //Exercice 1
-let jeune = 0; 
-let adulte = 0; 
-let centenaire= 0; 
+let jeune = 0;
+let adulte = 0;
+let centenaire = 0;
 let age = 0;
-while (age<100){
-     age = parseInt(prompt("Saisir un age"));
-    if(age<20){
-jeune++;
+while (age < 100) {
+    age = parseInt(prompt("Saisir un age"));
+    if (age < 20) {
+        jeune++;
     }
-    else if (age>=20 && age<=40){
+    else if (age >= 20 && age <= 40) {
         adulte++;
     }
     else {
@@ -16,9 +16,21 @@ jeune++;
         break;
     }
 }
-document.write("Nombre de personnes de moins de 20 ans : " +jeune+"<br>");
-document.write("Nombre de personnes entre 20 et 40 ans : " +adulte+"<br>");
-document.write("Nombre de personnes de plus de 40 ans : " +centenaire+"<br>");
+document.write("Nombre de personnes de moins de 20 ans : " + jeune + "<br>");
+document.write("Nombre de personnes entre 20 et 40 ans : " + adulte + "<br>");
+document.write("Nombre de personnes de plus de 40 ans : " + centenaire + "<br>");
+
+//Exercice 2
+let i = 0;
+let resultat = 0;
+let nb = parseInt(prompt("Saisir un nombre"));
+function TabledeMultiplication(nb) {
+    for (i = 0; i <= 10; i++) {
+        resultat = (nb * i);
+        document.write(nb + "*" + i + "=" + resultat + "<br>");
+    }
+}
+TabledeMultiplication(nb);
 
 
 
