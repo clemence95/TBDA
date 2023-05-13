d352ce2199b4ce3e2d3b6af2592de0e696a57385
boitelle clémence <boitelleclemence@outlook.fr>
2023-05-13 14:19:03 +0200

    html

--- html/evaluation_Javascript/script.js
+++ html/evaluation_Javascript/script.js
@@ -1,20 +1,20 @@
 //Exercice 1
-let age = prompt(perseInt("Saisir un age"));
 let jeune = 0; 
 let adulte = 0; 
 let centenaire= 0; 
-
-while(age<!20){
+let age = prompt(parseInt("Saisir un age"));
+while (age ==""){
+     age = prompt(parseInt("Saisir un age"));
+    if(age<20){
 jeune++;
-let age = prompt(perseInt("Saisir un age"));
-if(age>=20 && age>=40){
-    adulte++;
-     age = prompt(perseInt("Saisir un age"));
-}
-else if(age>!40){
-    centenaire++;
-    break;
-}
+    }
+    else if (age>=20 && age<=40){
+        adulte++;
+    }
+    else (age<100 && age>40) {
+        centenaire++;
+        break;
+    }
 }
 document.write("Nombre de personnes de moins de 20 ans : " +jeune+"<br>");
 document.write("Nombre de personnes entre 20 et 40 ans : " +adulte+"<br>");
