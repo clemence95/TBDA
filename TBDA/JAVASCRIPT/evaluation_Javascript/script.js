16bb477e5062ae57e69dcf3b06170222a755dee8
Boitelle <boitelleclemence@outlook.fr>
2023-05-15 13:41:03 +0200

    TBDA

--- TBDA/JAVASCRIPT/evaluation_Javascript/script.js
+++ TBDA/JAVASCRIPT/evaluation_Javascript/script.js
@@ -3,32 +3,32 @@ let jeune = 0;
 let adulte = 0;
 let centenaire = 0;
 let age = 0;
-while (age>40) {
-    if (age > 20) {
+while (age<41){
+    age = parseInt(prompt("Saisir un age"));
+    if (age < 20) {
         jeune++;
-        age = parseInt(prompt("Saisir un age"))
     }
     else if (age >= 20 && age <= 40) {
         adulte++;
-        age = parseInt(prompt("Saisir un age"))
     }
-    centenaire;
-    break;
 }
+centenaire++;
 document.write("Nombre de personnes de moins de 20 ans : " + jeune + "<br>");
 document.write("Nombre de personnes entre 20 et 40 ans : " + adulte + "<br>");
-document.write("Nombre de personnes de plus de 40 ans : " + centenaire + "<br><br>");
+document.write("Nombre de personnes de plus de 40 ans : " + centenaire + "<br>");
 
 //Exercice 2
 let i = 0;
 let nb = parseInt(prompt("Saisir un nombre"));
-function TableMultiplication(nb) {
+let resultat;
+function TableMultiplication(x) {
     for (i = 0; i <= 10; i++) {
-        resultat = nb * i;
+        resultat=(i*x);
+        document.write(x+"*"+i+"="+resultat+"<br>");
     }
 }
 TableMultiplication(nb);
-document.write(TableMultiplication(nb));
+
 
 
 
