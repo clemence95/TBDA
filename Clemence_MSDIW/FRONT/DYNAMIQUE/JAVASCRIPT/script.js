a0d08aa8ae37c73e356a6257ecb690df174b4137
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-07-07 11:56:23 +0200

    catégories html js caroussel

--- Clemence_MSDIW/FRONT/DYNAMIQUE/JAVASCRIPT/script.js
+++ Clemence_MSDIW/FRONT/DYNAMIQUE/JAVASCRIPT/script.js
@@ -21,4 +21,7 @@
 })()
 
 var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel)
+var carousel = new bootstrap.Carousel(myCarousel{
+  interval: 2000,
+  wrap: false
+})
