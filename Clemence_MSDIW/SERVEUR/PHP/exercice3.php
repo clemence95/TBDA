5b5d8c95bb4f16e668132935f6074b21564ea129
boitelle clémence <boitelleclemence@outlook.fr>
2023-07-12 20:33:57 +0200

    php

--- Clemence_MSDIW/SERVEUR/PHP/exercice3.php
+++ Clemence_MSDIW/SERVEUR/PHP/exercice3.php
@@ -1,286 +1,323 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LesTableaux</title>
 </head>
+
 <body>
     <?PHP
- $capitales = array(
-    "Bucarest" => "Roumanie",
-    "Bruxelles" => "Belgique",
-    "Oslo" => "Norvège",
-    "Ottawa" => "Canada",
-    "Paris" => "France",
-    "Port-au-Prince" => "Haïti",
-    "Port-d'Espagne" => "Trinité-et-Tobago",
-    "Prague" => "République tchèque",
-    "Rabat" => "Maroc",
-    "Riga" => "Lettonie",
-    "Rome" => "Italie",
-    "San José" => "Costa Rica",
-    "Santiago" => "Chili",
-    "Sofia" => "Bulgarie",
-    "Alger" => "Algérie",
-    "Amsterdam" => "Pays-Bas",
-    "Andorre-la-Vieille" => "Andorre",
-    "Asuncion" => "Paraguay",
-    "Athènes" => "Grèce",
-    "Bagdad" => "Irak",
-    "Bamako" => "Mali",
-    "Berlin" => "Allemagne",
-    "Bogota" => "Colombie",
-    "Brasilia" => "Brésil",
-    "Bratislava" => "Slovaquie",
-    "Varsovie" => "Pologne",
-    "Budapest" => "Hongrie",
-    "Le Caire" => "Egypte",
-    "Canberra" => "Australie",
-    "Caracas" => "Venezuela",
-    "Jakarta" => "Indonésie",
-    "Kiev" => "Ukraine",
-    "Kigali" => "Rwanda",
-    "Kingston" => "Jamaïque",
-    "Lima" => "Pérou",
-    "Londres" => "Royaume-Uni",
-    "Madrid" => "Espagne",
-    "Malé" => "Maldives",
-    "Mexico" => "Mexique",
-    "Minsk" => "Biélorussie",
-    "Moscou" => "Russie",
-    "Nairobi" => "Kenya",
-    "New Delhi" => "Inde",
-    "Stockholm" => "Suède",
-    "Téhéran" => "Iran",
-    "Tokyo" => "Japon",
-    "Tunis" => "Tunisie",
-    "Copenhague" => "Danemark",
-    "Dakar" => "Sénégal",
-    "Damas" => "Syrie",
-    "Dublin" => "Irlande",
-    "Erevan" => "Arménie",
-    "La Havane" => "Cuba",
-    "Helsinki" => "Finlande",
-    "Islamabad" => "Pakistan",
-    "Vienne" => "Autriche",
-    "Vilnius" => "Lituanie",
-    "Zagreb" => "Croatie"
-);
+    // Mois de l'année non bissextile
+    $mois = array(
+        "janvier" => 31,
+        "février" => 28,
+        "mars" => 31,
+        "avril" => 30,
+        "mai" => 31,
+        "juin" => 30,
+        "juillet" => 31,
+        "août" => 31,
+        "septembre" => 30,
+        "octobre" => 31,
+        "novembre" => 30,
+        "décembre" => 31
+    );
 
-// Trie le tableau par ordre alphabétique des capitales
-ksort($capitales);
+    // Affichage du tableau initial
+    echo "<h3>Tableau initial :</h3>";
+    echo "<table>";
+    echo "<tr><th>Mois</th><th>Nombre de jours</th></tr>";
 
-// Affiche la liste des capitales et des pays
-foreach ($capitales as $capitale => $pays) {
-    echo "$capitale - $pays<br>";
-}
-
-$capitales = array(
-    "Bucarest" => "Roumanie",
-    "Bruxelles" => "Belgique",
-    "Oslo" => "Norvège",
-    "Ottawa" => "Canada",
-    "Paris" => "France",
-    "Port-au-Prince" => "Haïti",
-    "Port-d'Espagne" => "Trinité-et-Tobago",
-    "Prague" => "République tchèque",
-    "Rabat" => "Maroc",
-    "Riga" => "Lettonie",
-    "Rome" => "Italie",
-    "San José" => "Costa Rica",
-    "Santiago" => "Chili",
-    "Sofia" => "Bulgarie",
-    "Alger" => "Algérie",
-    "Amsterdam" => "Pays-Bas",
-    "Andorre-la-Vieille" => "Andorre",
-    "Asuncion" => "Paraguay",
-    "Athènes" => "Grèce",
-    "Bagdad" => "Irak",
-    "Bamako" => "Mali",
-    "Berlin" => "Allemagne",
-    "Bogota" => "Colombie",
-    "Brasilia" => "Brésil",
-    "Bratislava" => "Slovaquie",
-    "Varsovie" => "Pologne",
-    "Budapest" => "Hongrie",
-    "Le Caire" => "Egypte",
-    "Canberra" => "Australie",
-    "Caracas" => "Venezuela",
-    "Jakarta" => "Indonésie",
-    "Kiev" => "Ukraine",
-    "Kigali" => "Rwanda",
-    "Kingston" => "Jamaïque",
-    "Lima" => "Pérou",
-    "Londres" => "Royaume-Uni",
-    "Madrid" => "Espagne",
-    "Malé" => "Maldives",
-    "Mexico" => "Mexique",
-    "Minsk" => "Biélorussie",
-    "Moscou" => "Russie",
-    "Nairobi" => "Kenya",
-    "New Delhi" => "Inde",
-    "Stockholm" => "Suède",
-    "Téhéran" => "Iran",
-    "Tokyo" => "Japon",
-    "Tunis" => "Tunisie",
-    "Copenhague" => "Danemark",
-    "Dakar" => "Sénégal",
-    "Damas" => "Syrie",
-    "Dublin" => "Irlande",
-    "Erevan" => "Arménie",
-    "La Havane" => "Cuba",
-    "Helsinki" => "Finlande",
-    "Islamabad" => "Pakistan",
-    "Vienne" => "Autriche",
-    "Vilnius" => "Lituanie",
-    "Zagreb" => "Croatie"
-);
-
-// Trie le tableau par ordre alphabétique des pays
-asort($capitales);
-
-// Affiche la liste des pays et des capitales
-foreach ($capitales as $capitale => $pays) {
-    echo "$pays - $capitale<br>";
-}
-$capitales = array(
-    "Bucarest" => "Roumanie",
-    "Bruxelles" => "Belgique",
-    "Oslo" => "Norvège",
-    "Ottawa" => "Canada",
-    "Paris" => "France",
-    "Port-au-Prince" => "Haïti",
-    "Port-d'Espagne" => "Trinité-et-Tobago",
-    "Prague" => "République tchèque",
-    "Rabat" => "Maroc",
-    "Riga" => "Lettonie",
-    "Rome" => "Italie",
-    "San José" => "Costa Rica",
-    "Santiago" => "Chili",
-    "Sofia" => "Bulgarie",
-    "Alger" => "Algérie",
-    "Amsterdam" => "Pays-Bas",
-    "Andorre-la-Vieille" => "Andorre",
-    "Asuncion" => "Paraguay",
-    "Athènes" => "Grèce",
-    "Bagdad" => "Irak",
-    "Bamako" => "Mali",
-    "Berlin" => "Allemagne",
-    "Bogota" => "Colombie",
-    "Brasilia" => "Brésil",
-    "Bratislava" => "Slovaquie",
-    "Varsovie" => "Pologne",
-    "Budapest" => "Hongrie",
-    "Le Caire" => "Egypte",
-    "Canberra" => "Australie",
-    "Caracas" => "Venezuela",
-    "Jakarta" => "Indonésie",
-    "Kiev" => "Ukraine",
-    "Kigali" => "Rwanda",
-    "Kingston" => "Jamaïque",
-    "Lima" => "Pérou",
-    "Londres" => "Royaume-Uni",
-    "Madrid" => "Espagne",
-    "Malé" => "Maldives",
-    "Mexico" => "Mexique",
-    "Minsk" => "Biélorussie",
-    "Moscou" => "Russie",
-    "Nairobi" => "Kenya",
-    "New Delhi" => "Inde",
-    "Stockholm" => "Suède",
-    "Téhéran" => "Iran",
-    "Tokyo" => "Japon",
-    "Tunis" => "Tunisie",
-    "Copenhague" => "Danemark",
-    "Dakar" => "Sénégal",
-    "Damas" => "Syrie",
-    "Dublin" => "Irlande",
-    "Erevan" => "Arménie",
-    "La Havane" => "Cuba",
-    "Helsinki" => "Finlande",
-    "Islamabad" => "Pakistan",
-    "Vienne" => "Autriche",
-    "Vilnius" => "Lituanie",
-    "Zagreb" => "Croatie"
-);
-
-$nombrePays = count($capitales);    //déterminer le nombre d'éléments présents dans le tableau
-
-echo "Le nombre de pays dans le tableau est : $nombrePays";
-$capitales = array(
-    "Bucarest" => "Roumanie",
-    "Bruxelles" => "Belgique",
-    "Oslo" => "Norvège",
-    "Ottawa" => "Canada",
-    "Paris" => "France",
-    "Port-au-Prince" => "Haïti",
-    "Port-d'Espagne" => "Trinité-et-Tobago",
-    "Prague" => "République tchèque",
-    "Rabat" => "Maroc",
-    "Riga" => "Lettonie",
-    "Rome" => "Italie",
-    "San José" => "Costa Rica",
-    "Santiago" => "Chili",
-    "Sofia" => "Bulgarie",
-    "Alger" => "Algérie",
-    "Amsterdam" => "Pays-Bas",
-    "Andorre-la-Vieille" => "Andorre",
-    "Asuncion" => "Paraguay",
-    "Athènes" => "Grèce",
-    "Bagdad" => "Irak",
-    "Bamako" => "Mali",
-    "Berlin" => "Allemagne",
-    "Bogota" => "Colombie",
-    "Brasilia" => "Brésil",
-    "Bratislava" => "Slovaquie",
-    "Varsovie" => "Pologne",
-    "Budapest" => "Hongrie",
-    "Le Caire" => "Egypte",
-    "Canberra" => "Australie",
-    "Caracas" => "Venezuela",
-    "Jakarta" => "Indonésie",
-    "Kiev" => "Ukraine",
-    "Kigali" => "Rwanda",
-    "Kingston" => "Jamaïque",
-    "Lima" => "Pérou",
-    "Londres" => "Royaume-Uni",
-    "Madrid" => "Espagne",
-    "Malé" => "Maldives",
-    "Mexico" => "Mexique",
-    "Minsk" => "Biélorussie",
-    "Moscou" => "Russie",
-    "Nairobi" => "Kenya",
-    "New Delhi" => "Inde",
-    "Stockholm" => "Suède",
-    "Téhéran" => "Iran",
-    "Tokyo" => "Japon",
-    "Tunis" => "Tunisie",
-    "Copenhague" => "Danemark",
-    "Dakar" => "Sénégal",
-    "Damas" => "Syrie",
-    "Dublin" => "Irlande",
-    "Erevan" => "Arménie",
-    "La Havane" => "Cuba",
-    "Helsinki" => "Finlande",
-    "Islamabad" => "Pakistan",
-    "Vienne" => "Autriche",
-    "Vilnius" => "Lituanie",
-    "Zagreb" => "Croatie"
-);
-
-foreach ($capitales as $capitale => $pays) {    //examine chaque élément du tableau.
-    if (substr($capitale, 0, 1) !== 'B') {      //qui extrait une sous-chaîne de caractères
-        unset($capitales[$capitale]);           //est utilisée pour supprimer l'élément correspondant du tableau
+    foreach ($mois as $m => $jours) {
+        echo "<tr><td>$m</td><td>$jours</td></tr>";     //est utilisée pour parcourir chaque élément du tableau
     }
-}
 
-foreach ($capitales as $capitale => $pays) {
-    echo "$capitale : $pays <br>";  //affiche la capitale suivie du pays correspondant
-}
+    echo "</table>";
+    echo "<br>";
+
+    //Capitales
+    $capitales = array(
+        "Bucarest" => "Roumanie",
+        "Bruxelles" => "Belgique",
+        "Oslo" => "Norvège",
+        "Ottawa" => "Canada",
+        "Paris" => "France",
+        "Port-au-Prince" => "Haïti",
+        "Port-d'Espagne" => "Trinité-et-Tobago",
+        "Prague" => "République tchèque",
+        "Rabat" => "Maroc",
+        "Riga" => "Lettonie",
+        "Rome" => "Italie",
+        "San José" => "Costa Rica",
+        "Santiago" => "Chili",
+        "Sofia" => "Bulgarie",
+        "Alger" => "Algérie",
+        "Amsterdam" => "Pays-Bas",
+        "Andorre-la-Vieille" => "Andorre",
+        "Asuncion" => "Paraguay",
+        "Athènes" => "Grèce",
+        "Bagdad" => "Irak",
+        "Bamako" => "Mali",
+        "Berlin" => "Allemagne",
+        "Bogota" => "Colombie",
+        "Brasilia" => "Brésil",
+        "Bratislava" => "Slovaquie",
+        "Varsovie" => "Pologne",
+        "Budapest" => "Hongrie",
+        "Le Caire" => "Egypte",
+        "Canberra" => "Australie",
+        "Caracas" => "Venezuela",
+        "Jakarta" => "Indonésie",
+        "Kiev" => "Ukraine",
+        "Kigali" => "Rwanda",
+        "Kingston" => "Jamaïque",
+        "Lima" => "Pérou",
+        "Londres" => "Royaume-Uni",
+        "Madrid" => "Espagne",
+        "Malé" => "Maldives",
+        "Mexico" => "Mexique",
+        "Minsk" => "Biélorussie",
+        "Moscou" => "Russie",
+        "Nairobi" => "Kenya",
+        "New Delhi" => "Inde",
+        "Stockholm" => "Suède",
+        "Téhéran" => "Iran",
+        "Tokyo" => "Japon",
+        "Tunis" => "Tunisie",
+        "Copenhague" => "Danemark",
+        "Dakar" => "Sénégal",
+        "Damas" => "Syrie",
+        "Dublin" => "Irlande",
+        "Erevan" => "Arménie",
+        "La Havane" => "Cuba",
+        "Helsinki" => "Finlande",
+        "Islamabad" => "Pakistan",
+        "Vienne" => "Autriche",
+        "Vilnius" => "Lituanie",
+        "Zagreb" => "Croatie"
+    );
+
+    // Trie le tableau par ordre alphabétique des capitales
+    ksort($capitales);
+
+    // Affiche la liste des capitales et des pays
+    foreach ($capitales as $capitale => $pays) {
+        echo "$capitale - $pays<br>";
+    }
+    echo "<br>";
+
+    $capitales = array(
+        "Bucarest" => "Roumanie",
+        "Bruxelles" => "Belgique",
+        "Oslo" => "Norvège",
+        "Ottawa" => "Canada",
+        "Paris" => "France",
+        "Port-au-Prince" => "Haïti",
+        "Port-d'Espagne" => "Trinité-et-Tobago",
+        "Prague" => "République tchèque",
+        "Rabat" => "Maroc",
+        "Riga" => "Lettonie",
+        "Rome" => "Italie",
+        "San José" => "Costa Rica",
+        "Santiago" => "Chili",
+        "Sofia" => "Bulgarie",
+        "Alger" => "Algérie",
+        "Amsterdam" => "Pays-Bas",
+        "Andorre-la-Vieille" => "Andorre",
+        "Asuncion" => "Paraguay",
+        "Athènes" => "Grèce",
+        "Bagdad" => "Irak",
+        "Bamako" => "Mali",
+        "Berlin" => "Allemagne",
+        "Bogota" => "Colombie",
+        "Brasilia" => "Brésil",
+        "Bratislava" => "Slovaquie",
+        "Varsovie" => "Pologne",
+        "Budapest" => "Hongrie",
+        "Le Caire" => "Egypte",
+        "Canberra" => "Australie",
+        "Caracas" => "Venezuela",
+        "Jakarta" => "Indonésie",
+        "Kiev" => "Ukraine",
+        "Kigali" => "Rwanda",
+        "Kingston" => "Jamaïque",
+        "Lima" => "Pérou",
+        "Londres" => "Royaume-Uni",
+        "Madrid" => "Espagne",
+        "Malé" => "Maldives",
+        "Mexico" => "Mexique",
+        "Minsk" => "Biélorussie",
+        "Moscou" => "Russie",
+        "Nairobi" => "Kenya",
+        "New Delhi" => "Inde",
+        "Stockholm" => "Suède",
+        "Téhéran" => "Iran",
+        "Tokyo" => "Japon",
+        "Tunis" => "Tunisie",
+        "Copenhague" => "Danemark",
+        "Dakar" => "Sénégal",
+        "Damas" => "Syrie",
+        "Dublin" => "Irlande",
+        "Erevan" => "Arménie",
+        "La Havane" => "Cuba",
+        "Helsinki" => "Finlande",
+        "Islamabad" => "Pakistan",
+        "Vienne" => "Autriche",
+        "Vilnius" => "Lituanie",
+        "Zagreb" => "Croatie"
+    );
+
+    // Trie le tableau par ordre alphabétique des pays
+    asort($capitales);
+
+    // Affiche la liste des pays et des capitales
+    foreach ($capitales as $capitale => $pays) {
+        echo "$pays - $capitale<br>";
+    }
+    echo "<br>";
+
+    $capitales = array(
+        "Bucarest" => "Roumanie",
+        "Bruxelles" => "Belgique",
+        "Oslo" => "Norvège",
+        "Ottawa" => "Canada",
+        "Paris" => "France",
+        "Port-au-Prince" => "Haïti",
+        "Port-d'Espagne" => "Trinité-et-Tobago",
+        "Prague" => "République tchèque",
+        "Rabat" => "Maroc",
+        "Riga" => "Lettonie",
+        "Rome" => "Italie",
+        "San José" => "Costa Rica",
+        "Santiago" => "Chili",
+        "Sofia" => "Bulgarie",
+        "Alger" => "Algérie",
+        "Amsterdam" => "Pays-Bas",
+        "Andorre-la-Vieille" => "Andorre",
+        "Asuncion" => "Paraguay",
+        "Athènes" => "Grèce",
+        "Bagdad" => "Irak",
+        "Bamako" => "Mali",
+        "Berlin" => "Allemagne",
+        "Bogota" => "Colombie",
+        "Brasilia" => "Brésil",
+        "Bratislava" => "Slovaquie",
+        "Varsovie" => "Pologne",
+        "Budapest" => "Hongrie",
+        "Le Caire" => "Egypte",
+        "Canberra" => "Australie",
+        "Caracas" => "Venezuela",
+        "Jakarta" => "Indonésie",
+        "Kiev" => "Ukraine",
+        "Kigali" => "Rwanda",
+        "Kingston" => "Jamaïque",
+        "Lima" => "Pérou",
+        "Londres" => "Royaume-Uni",
+        "Madrid" => "Espagne",
+        "Malé" => "Maldives",
+        "Mexico" => "Mexique",
+        "Minsk" => "Biélorussie",
+        "Moscou" => "Russie",
+        "Nairobi" => "Kenya",
+        "New Delhi" => "Inde",
+        "Stockholm" => "Suède",
+        "Téhéran" => "Iran",
+        "Tokyo" => "Japon",
+        "Tunis" => "Tunisie",
+        "Copenhague" => "Danemark",
+        "Dakar" => "Sénégal",
+        "Damas" => "Syrie",
+        "Dublin" => "Irlande",
+        "Erevan" => "Arménie",
+        "La Havane" => "Cuba",
+        "Helsinki" => "Finlande",
+        "Islamabad" => "Pakistan",
+        "Vienne" => "Autriche",
+        "Vilnius" => "Lituanie",
+        "Zagreb" => "Croatie"
+    );
+
+    $nombrePays = count($capitales);    //déterminer le nombre d'éléments présents dans le tableau
+
+    echo "Le nombre de pays dans le tableau est : $nombrePays";
+    echo "<br>";
+    
+    $capitales = array(
+        "Bucarest" => "Roumanie",
+        "Bruxelles" => "Belgique",
+        "Oslo" => "Norvège",
+        "Ottawa" => "Canada",
+        "Paris" => "France",
+        "Port-au-Prince" => "Haïti",
+        "Port-d'Espagne" => "Trinité-et-Tobago",
+        "Prague" => "République tchèque",
+        "Rabat" => "Maroc",
+        "Riga" => "Lettonie",
+        "Rome" => "Italie",
+        "San José" => "Costa Rica",
+        "Santiago" => "Chili",
+        "Sofia" => "Bulgarie",
+        "Alger" => "Algérie",
+        "Amsterdam" => "Pays-Bas",
+        "Andorre-la-Vieille" => "Andorre",
+        "Asuncion" => "Paraguay",
+        "Athènes" => "Grèce",
+        "Bagdad" => "Irak",
+        "Bamako" => "Mali",
+        "Berlin" => "Allemagne",
+        "Bogota" => "Colombie",
+        "Brasilia" => "Brésil",
+        "Bratislava" => "Slovaquie",
+        "Varsovie" => "Pologne",
+        "Budapest" => "Hongrie",
+        "Le Caire" => "Egypte",
+        "Canberra" => "Australie",
+        "Caracas" => "Venezuela",
+        "Jakarta" => "Indonésie",
+        "Kiev" => "Ukraine",
+        "Kigali" => "Rwanda",
+        "Kingston" => "Jamaïque",
+        "Lima" => "Pérou",
+        "Londres" => "Royaume-Uni",
+        "Madrid" => "Espagne",
+        "Malé" => "Maldives",
+        "Mexico" => "Mexique",
+        "Minsk" => "Biélorussie",
+        "Moscou" => "Russie",
+        "Nairobi" => "Kenya",
+        "New Delhi" => "Inde",
+        "Stockholm" => "Suède",
+        "Téhéran" => "Iran",
+        "Tokyo" => "Japon",
+        "Tunis" => "Tunisie",
+        "Copenhague" => "Danemark",
+        "Dakar" => "Sénégal",
+        "Damas" => "Syrie",
+        "Dublin" => "Irlande",
+        "Erevan" => "Arménie",
+        "La Havane" => "Cuba",
+        "Helsinki" => "Finlande",
+        "Islamabad" => "Pakistan",
+        "Vienne" => "Autriche",
+        "Vilnius" => "Lituanie",
+        "Zagreb" => "Croatie"
+    );
+
+    foreach ($capitales as $capitale => $pays) {    //examine chaque élément du tableau.
+        if (substr($capitale, 0, 1) !== 'B') {      //qui extrait une sous-chaîne de caractères
+            unset($capitales[$capitale]);           //est utilisée pour supprimer l'élément correspondant du tableau
+        }
+    }
+
+    foreach ($capitales as $capitale => $pays) {
+        echo "$capitale : $pays <br>";  //affiche la capitale suivie du pays correspondant
+    }
 
 
-?>
+    ?>
 
 </body>
+
 </html>
