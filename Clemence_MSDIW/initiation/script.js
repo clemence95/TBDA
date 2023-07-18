20c5cca228afd9d5dd97fcd2d31ac81daeab5529
boitelle clémence <boitelleclemence@outlook.fr>
2023-07-18 11:07:01 +0200

    js initiation

--- Clemence_MSDIW/initiation/script.js
+++ Clemence_MSDIW/initiation/script.js
@@ -3,4 +3,5 @@ document.getElementById('titre').style.color = "green";
  /* Selection de l'élément à partir de son id puis accès à la propriété color et enfin la nouvelle valeur (green) */
  document.getElementById('titre') 
  const titre = document.getElementById('titre'); 
+ /* Mise en place de l'élément dans une mémoire temporaire appelée ici "titre" */
 console.log(titre);
