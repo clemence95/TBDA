a81e072be921bbd86bbe8ed3dadfec58b89c8624
boitelle clémence <boitelleclemence@outlook.fr>
2023-07-18 11:13:36 +0200

    js

--- Clemence_MSDIW/initiation/script.js
+++ Clemence_MSDIW/initiation/script.js
@@ -4,4 +4,7 @@ document.getElementById('titre').style.color = "green";
  document.getElementById('titre') 
  const titre = document.getElementById('titre'); 
  /* Mise en place de l'élément dans une mémoire temporaire appelée ici "titre" */
+ /* ATTENTION il ne peut pas y avoir deux variables du même nom */
+ titre.style.color = "green" 
+ /* deux façons d'initialiser (de créer) une variable : let et const. */ 
 console.log(titre);
