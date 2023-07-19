a64e20fac6c61dbe06d4d8b968a0b2ef1fc8535d
Boitelle Clémence <boitelleclemence@outlook.fr>
2023-07-19 16:19:15 +0200

    php

---
+++ Clemence_MSDIW/SERVEUR/PHP/header.php
@@ -0,0 +1,21 @@
+    <header class="container overflow-hidden">
+        <div class="row d-flex align-items-center">
+            <nav class="navbar d-flex">
+                <a href="#" class="navbar-logo"><img class="img-fluid" id="img-logo" alt="img-logo"
+                        src="../assets/img/logo_transparent.png"></a>
+                <div class="navbar-right align-items-center">
+                    <a href="../HTML/accueil1.html">Accueil</a>
+                    <a href="../HTML/categories.html">Catégories</a>
+                    <a href="../HTML/Plats.html">Plats</a>
+                    <a href="../HTML/formulaire.html">Contacte</a>
+                    <a href="../HTML/commande.html">Commande</a>
+                </div>
+                <div class="col-md-1"><input placeholder="Votre recherche" id="search" type="search"></div>
+            </nav>
+            <div class="video container-fluid">
+                <video autoplay loop muted id="video-bg">
+                    <source src="../assets/pizza-fond.mp4">
+                </video>
+            </div>
+        </div>
+    </header>
