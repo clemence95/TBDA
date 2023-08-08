178224274ee350188bf08196ee5bc94920999136
boitelle clémence <boitelleclemence@outlook.fr>
2023-08-08 23:46:50 +0200

    boots, php

---
+++ Clemence_MSDIW/SERVEUR/PHP/nav.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TheDestrit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lugrasimo&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../CSS/style2.css">
+</head>
+
+<body>
+    <nav class="cc-navbar fixed-top navbar navbar-expand-lg navbar-dark md-3 w-100">
+        <div class="container-fluid">
+            <a class="navbar-brand text-uppercase mx-4 py-3" href="#">The Destrict</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-3 mb-lg-0 sm-3">
+                    <li class="nav-item pe-4">
+                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item pe-4">
+                        <a class="nav-link" href="#">Catégorie</a>
+                    </li>
+                    <li class="nav-item pe-4">
+                        <a class="nav-link" href="#">Plats</a>
+                    </li>
+                    <li class="nav-item pe-4">
+                        <a class="nav-link" href="..\HTML\formulaire.html">Nous Contacter</a>
+                    </li>
+                    <li class="nav-item pe-4">
+                        <a class="btn btn-order rounded-1" href="#">Passer la commande</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Recherche</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    </body>
+
+</html>
