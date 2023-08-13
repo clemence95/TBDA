101f662b5e0b487421e8d3bc9bc4a2b8f0dc6313
boitelle clémence <boitelleclemence@outlook.fr>
2023-08-13 17:31:29 +0200

    footer php

---
+++ Clemence_MSDIW/Projet-theDistrict/assets/php/footer.php
@@ -0,0 +1,19 @@
+<footer>
+        <div clas="container">
+            <div class="row">
+                <div class="footer fixed-bottom md-3 w-100 bg-dark-subtil">
+                    <ul class="list-inline text-center">
+                        <li class="list-inline-item">
+                            <a href="#"><i class="bi bi-facebook"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#"><i class="bi bi-geo-alt"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#"><i class="bi bi-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
