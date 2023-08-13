de4d918ded59e4d270cb77171593d8fcbe70e24a
boitelle clémence <boitelleclemence@outlook.fr>
2023-08-13 18:31:59 +0200

    php - projet

--- Clemence_MSDIW/Projet-theDistrict/assets/contacte.php
+++ Clemence_MSDIW/Projet-theDistrict/assets/contacte.php
@@ -15,44 +15,58 @@
 </head>
 
 <body>
-    <?php
-    include './php/nav.php';
-    ?>
+    <nav>
+        <?php
+        include './php/nav.php';
+        ?>
+    </nav>
+   <?php
+   include './php/banner.php'
+   ?>
     <form class=" row g-3 needs-validation" novalidate>
-    <div class="col-md-3 mb-4">
-      <input type="text" class="form-control" placeholder="Prénom" aria-label="prenom" id="validationCustom01" required>
-      <div class="invalid-feedback">
-        Vous devez saisir votre Prénom
-      </div>
-    </div>
-    <div class="col-md-3 mb-4">
-      <input type="text" class="form-control" placeholder="Nom" aria-label="nom" id="validationCustom02" required>
-      <div class="invalid-feedback">
-        Vous devez saisir votre Nom
-      </div>
-    </div>
-    <div class="row g-3">
-      <div class="col-md-4 mb-4">
-        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" required>
-        <div class="invalid-feedback">
-          Saisir une adresse email valide
+        <div class="col-md-3 mb-4">
+            <input type="text" class="form-control" placeholder="Prénom" aria-label="prenom" id="validationCustom01" required>
+            <div class="invalid-feedback">
+                Vous devez saisir votre Prénom
+            </div>
         </div>
-      </div>
-      <div class="col-md-4 mb-4">
-        <input type="tel" class="form-control" placeholder="Téléphone" id="exampleInputPassword1" required>
-        <div class="invalid-feedback">
-          Saisir votre numéro de téléphone
+        <div class="col-md-3 mb-4">
+            <input type="text" class="form-control" placeholder="Nom" aria-label="nom" id="validationCustom02" required>
+            <div class="invalid-feedback">
+                Vous devez saisir votre Nom
+            </div>
         </div>
-      </div>
-    </div>
-    <div class="col-md-10">
-      <textarea class="form-control" placeholder="Votre demande" id="floatingTextarea2" style="height: 100px"
-        required></textarea>
-      <div class="invalid-feedback">
-        Saisir votre demande
-      </div>
-    </div>
-    <div class="col-md-10">
-      <button type="submit" class="btn btn-primary" id="valide">Valider</button>
-    </div>
-  </form>
+        <div class="row g-3">
+            <div class="col-md-4 mb-4">
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email" required>
+                <div class="invalid-feedback">
+                    Saisir une adresse email valide
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <input type="tel" class="form-control" placeholder="Téléphone" id="exampleInputPassword1" required>
+                <div class="invalid-feedback">
+                    Saisir votre numéro de téléphone
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10">
+            <textarea class="form-control" placeholder="Votre demande" id="floatingTextarea2" style="height: 100px" required></textarea>
+            <div class="invalid-feedback">
+                Saisir votre demande
+            </div>
+        </div>
+        <div class="col-md-10">
+            <button type="submit" class="btn btn-primary" id="valide">Valider</button>
+        </div>
+    </form>
+
+    <footer>
+        <?php
+        include './php/footer.php';
+        ?>
+    </footer>
+    <script src="/Clemence_MSDIW/FRONT/DYNAMIQUE/JAVASCRIPT/script.js"></script>
+</body>
+
+</html>
---
+++ Clemence_MSDIW/Projet-theDistrict/assets/php/banner.php
@@ -0,0 +1,22 @@
+<section class="banner d-flex pt-5">
+        <div class="container-fluid my-5 py-4 d-flex justify-content-center">
+            <div class="row ">
+                <div class=" video-container d-flex justify-content-center">
+                    <video id="myVideo" class="d-block video rounded-5 " autoplay muted loop>
+                        <source src="/Clemence_MSDIW/Projet-theDistrict/assets/img-thedistrict/pizza-fond.mp4" type="video/mp4">
+                        Votre navigateur ne prend pas en charge la lecture de vidéos HTML5.
+                    </video>
+                </div>
+                <div class="col-md-12">
+                    <h1 class="text-center dancing">The Destrict</h1>
+                </div>
+                <div class="col-md-6">
+                    <p class="text-capitalize banner-desc">Lorem ipsum dolor sit amet consectetur adipisicing elit Vitae
+                        officiis accusamus</p><br>
+                    <p>
+                        <buttom class="btn btn-order btn-lg me-5 dancing">Commander maintenant</buttom>
+                        <button class="btn btn-outline-info dancing">Réserver</button>
+                </div>
+            </div>
+        </div>
+    </section>
