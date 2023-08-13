f5c85be62e7399d18820a6d0f845ff82f67975d3
boitelle clémence <boitelleclemence@outlook.fr>
2023-08-13 17:46:21 +0200

    php

--- Clemence_MSDIW/Projet-theDistrict/accueil.php
+++ Clemence_MSDIW/Projet-theDistrict/accueil.php
@@ -10,6 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lugrasimo&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
@@ -136,7 +137,79 @@
                 </div>
             </div>
     </section>
-
+    <section class="cc-menu py-5 dancing text-light ">
+        <div class="container">
+            <div class="row">
+                <h3 class="text-center text-light Lugrasimo mb-4">Nos Variétés</h3>
+                <div class="card bg-transparent text-center">
+                    <div class="card-header redressed fs-4">
+                        <ul class="nav nav-tabs justify-content-center card-header-tabs">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="true" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="#">Link</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="#">Disabled</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body text-light">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row row-cols-1 row-cols-md-2 g-4">
+            <div class="col">
+                <div class="card">
+                    <img src="../assets/img/cheesburger.jpg" class="card-img-top" alt="cheesburger">
+                    <div class="card-body">
+                        <h5 class="card-title">Cheesburger</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="../assets/img/cesar_salad.jpg" class="card-img-top" alt="cesar">
+                    <div class="card-body">
+                        <h5 class="card-title">Salade Cesar</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="../assets/img/courgettes_farcies.jpg" class="card-img-top" alt="courgette">
+                    <div class="card-body">
+                        <h5 class="card-title">Courgettes Farcies</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="../assets/img/spaghetti-legumes.jpg" class="card-img-top" alt="spaghetti-legumes">
+                    <div class="card-body">
+                        <h5 class="card-title">Spaghetti-legumes</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<footer>
+    <?php
+    include './assets/php/footer.php';
+    ?>
+</footer>
 
 </body>
 
