cc567ad44f12052a127fd8e743121a5e7ffb22b5
boitelle clémence <boitelleclemence@outlook.fr>
2023-08-18 00:00:08 +0200

    css html

--- Clemence_MSDIW/Projet-theDistrict/assets/php/banner.php
+++ Clemence_MSDIW/Projet-theDistrict/assets/php/banner.php
@@ -1,16 +1,14 @@
-<section class="banner d-flex pt-5">
+<section class="banner d-flex pt-5" style="width: 100%; position: relative;">
     <div class="container-fluid my-5 py-4 d-flex justify-content-center align-items-center">
-        <div class="row ">
-            <div class=" video-container d-flex justify-content-center">
-                <video id="myVideo" class="d-block video rounded-5 " autoplay muted loop>
-                    <source src="/Clemence_MSDIW/Projet-theDistrict/assets/img-thedistrict/pizza-fond.mp4" type="video/mp4">
-                    Votre navigateur ne prend pas en charge la lecture de vidéos HTML5.
-                </video>
-            </div>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Recherche</button>
-            </form>
+        <div class="video-container d-flex justify-content-center" style="width: 100%;">
+            <video id="myVideo" class="d-block video rounded-5" autoplay muted loop style="width: 100%;">
+                <source src="/Clemence_MSDIW/Projet-theDistrict/assets/img-thedistrict/pizza-fond.mp4" type="video/mp4">
+                Votre navigateur ne prend pas en charge la lecture de vidéos HTML5.
+            </video>
         </div>
+        <form class="d-flex" role="search" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+            <input class="form-control me-2" type="search" placeholder="Rechercher" aria-label="Search"  style="width: 50vh">
+            <button class="btn btn-outline-success" type="submit">Recherche</button>
+        </form>
     </div>
 </section>
